3076243728ee3c6d3f32c35756e96fc02240577e
KirillDmit <kirilldmitriev20015@gmail.com>
2022-11-14 00:20:32 +0500

    Init

---
+++ server/src/main/java/ru/autohelp/server/controllers/Controller.java
@@ -0,0 +1,13 @@
+package ru.autohelp.server.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello!";
+    }
+}
