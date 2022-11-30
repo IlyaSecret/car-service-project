6ba9f6a83ceaa343271597f8d309b2751071fe72
KirillDmit <kirilldmitriev20015@gmail.com>
2022-11-30 12:58:37 +0500

    admin

---
+++ server/src/main/java/ru/autohelp/server/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package ru.autohelp.server.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.autohelp.server.models.Car;
+import ru.autohelp.server.models.Client;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("admin")
+                .antMatchers("/auth/login","/auth/registration", "/error").permitAll()
+                .anyRequest().hasAnyRole("user","admin")
+//                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/people", true)
+                .failureUrl("/auth/login?error")
+                .and()
+                .logout().logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ server/src/main/java/ru/autohelp/server/controllers/AdminController.java
@@ -0,0 +1,20 @@
+package ru.autohelp.server.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping()
+    public String adminPage() {
+        return "admin/page";
+    }
+
+    @GetMapping("/auth")
+    public String reg() {
+        return "auth/login";
+    }
+}
