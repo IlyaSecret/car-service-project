ca8356c0ddd3da577b8b66cecb265ecf11fe6cef
KirillDmit <kirilldmitriev20015@gmail.com>
2022-12-18 20:44:03 +0500

    props

--- server/src/test/java/ru/autohelp/server/ServerApplicationTests.java
+++ server/src/test/java/ru/autohelp/server/ServerApplicationTests.java
@@ -2,12 +2,14 @@ package ru.autohelp.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
-class ServerApplicationTests {
+class ServerApplicationTests{
 
 	@Test
 	void contextLoads() {
+		Assert.notEmpty(new Object[]{1, "1", '1'});
 	}
 
 }
