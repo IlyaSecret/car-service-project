7d6f0c431fd06e7ffbb16ae2ffb10113fcbbf283
KirillDmit <kirilldmitriev20015@gmail.com>
2023-01-05 16:59:20 +0500

    Telegram Bot Config

--- server/src/main/java/ru/autohelp/server/service/TelegramBot.java
+++ server/src/main/java/ru/autohelp/server/service/TelegramBot.java
@@ -1,62 +1,61 @@
 package ru.autohelp.server.service;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.apache.log4j.Logger;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.api.objects.Update;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.autohelp.server.config.BotConfig;
 
-@AllArgsConstructor
-@NoArgsConstructor
+@Component
 public class TelegramBot extends TelegramLongPollingBot{
 
-    private static final Logger log = Logger.getLogger(TelegramBot.class);
+    private final BotConfig config;
 
-    private final int RECONNECT_PAUSE = 10000;
-
-    @Setter
-    @Getter
-    private String userName;
-
-    @Setter
-    @Getter
-    private String token;
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        log.debug("new Update receive");
+    public TelegramBot(BotConfig config){
+        this.config = config;
     }
 
     @Override
     public String getBotUsername() {
-        return userName;
+        return config.getBotUsername();
     }
 
     @Override
     public String getBotToken() {
-        return token;
+        return config.getBotToken();
     }
 
-    public void botConnect(){
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        try{
-            telegramBotsApi.registerBot(this);
-            log.info("TelegramAPI started. Look for messages");
-        } catch (TelegramApiRequestException e) {
-            log.error("Cant Connect. Pause " + RECONNECT_PAUSE / 1000 + "sec and try again. Error: " + e.getMessage());
-            try {
-                Thread.sleep(RECONNECT_PAUSE);
-            } catch (InterruptedException e1) {
-                e1.printStackTrace();
-                return;
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (messageText){
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, command was not recognized");
             }
-            botConnect();
         }
     }
 
+    private void startCommandReceived(long charId, String name){
+        String answer = "Hi, " + name + ", nice to meet you!";
+        sendMessage(charId, answer);
+    }
 
+    private void sendMessage(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try{
+            execute(message);
+        }
+        catch (TelegramApiException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
